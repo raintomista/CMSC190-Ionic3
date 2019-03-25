1be078ba825de5f58c1064bf192ba53687f8c0f6
Rain Tomista <rstomista@up.edu.ph>
2019-03-25 08:18:05 +0800

    Fix socket issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { HomePage } from './../pages/home/home';
 import { ProjectPage } from '../pages/project/project';
 import { ReviewComponentsPage } from './../pages/review-components/review-components';
 import { ScreenTabsPage } from './../pages/screen-tabs/screen-tabs';
+import { Socket } from 'ng-socket-io';
 
 @Component({
   templateUrl: 'app.html'
@@ -19,9 +20,12 @@ export class MyApp {
   constructor(
     public nativeStorage: NativeStorage,
     public platform: Platform,
+    public socket: Socket,
     public statusBar: StatusBar,
     public splashScreen: SplashScreen) {
       this.platform.ready().then(() => {
+        this.socket.connect();
+
         nativeStorage.getItem('facebook_user')
           .then((data) => {
             if(typeof(data) === 'undefined') {
@@ -39,6 +43,16 @@ export class MyApp {
           });
 
       });
+
+
+
+    this.platform.pause.subscribe(() => {
+      this.socket.disconnect()
+    });
+
+    this.platform.resume.subscribe(() => {
+      this.socket.connect()
+    })
   }
 }
 
